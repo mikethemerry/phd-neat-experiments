91ebfa22094cca6702a5fef4f7a1bf8d9e85f90c
Mike Merry <me@mikemerry.nz>
2019-06-30 16:41:10 +1200

    update runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -3,6 +3,7 @@
 docker run \
     --name explaneatGPU \
     --mount type=bind,source="$(pwd)",target=/root/app \
+    --mount type=bind,source="$(pwd)"/../../data,target=/root/data \
     --runtime=nvidia\
     --rm \
     -p 9999:9999 \
