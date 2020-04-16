9f62adf6aaa18141bf138045bb3fefd62fc26d0d
Mike Merry <me@mikemerry.nz>
2020-04-17 10:03:03 +1200

    getting everything back up to date

--- runDocker.sh
+++ runDocker.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 docker run \
-    --name explaneatGPU \
+    --name explaneatGPU2 \
     --mount type=bind,source="$(pwd)",target=/root/app \
     --mount type=bind,source="$(pwd)"/../../data,target=/root/data \
     --runtime=nvidia\
     --rm \
     -p 9999:9999 \
     -it \
-    mikethemerry/explaneatgpu ./app/runJupyter.sh
+    mikethemerry/explaneat-gpu-2 ./app/runJupyter.sh
