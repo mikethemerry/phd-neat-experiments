4c4a68006652f09bfa6323d184afe7708ef85a9a
Mike Merry <me@mikemerry.nz>
2022-06-02 09:31:37 +1200

    docker rebuild adjustments

--- rebuildDocker.sh
+++ rebuildDocker.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 cd dockers/explaneatgpu
+#cp dockers/explaneatgpu/dockerfile ../
+#cd ../
 docker build -t mikethemerry/explaneat-gpu-2 .
-cd ../..
+cd ../../
+# rm dockerfile
+# cd phd-neat-experiments
