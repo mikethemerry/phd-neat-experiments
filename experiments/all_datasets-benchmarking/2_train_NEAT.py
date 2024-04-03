55fb5a328c347f05c4ab93bcd11bc0510de030dd
Mike Merry <merry.mike@gmail.com>
2024-04-03 20:58:09 +1300

    adjust for function signatures

--- experiments/all_datasets-benchmarking/2_train_NEAT.py
+++ experiments/all_datasets-benchmarking/2_train_NEAT.py
@@ -135,7 +135,7 @@ def instantiate_population(config, xs, ys):
 
 def eval_genomes(genomes, config):
     ## evaluate the genomes using binary cross entropy
-    binary_cross_entropy(genomes, config, X_train, y_train, "cpu")
+    binary_cross_entropy({g.id: g for g in genomes}, config, X_train, y_train, "cpu")
 
 
 # ------------------- instantiate model ------------------------------
