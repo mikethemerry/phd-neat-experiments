8ca4d9741820709aa899f8a672c581ee08ff2fff
Mike Merry <merry.mike@gmail.com>
2024-04-03 21:12:59 +1300

    Tidy debug

--- experiments/all_datasets-benchmarking/2_train_NEAT.py
+++ experiments/all_datasets-benchmarking/2_train_NEAT.py
@@ -156,8 +156,6 @@ def my_binary_cross_entropy(genomes, config, xs, ys, device):
         # Convert preds to dtype Double using ndarray
         preds = np.array(preds, dtype=np.double)
         ys = np.array(ys, dtype=np.double)
-        print(type(preds[0].dtype))
-        print(type(ys[0].dtype))
 
         genome.fitness = float(
             1.0 / loss(torch.tensor(preds).to(device), torch.tensor(ys))
