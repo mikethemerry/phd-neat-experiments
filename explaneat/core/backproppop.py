197c00826183e1bed65b9a95a7966cd5f7ea9b4b
Mike Merry <merry.mike@gmail.com>
2024-09-05 09:37:41 +1200

    Turn on cuda

--- explaneat/core/backproppop.py
+++ explaneat/core/backproppop.py
@@ -1,4 +1,5 @@
 """Implements the core evolution algorithm."""
+
 from __future__ import print_function
 
 import sys
@@ -40,21 +41,23 @@ class BackpropPopulation(Population):
     This class extends the core NEAT implementation with a backprop method
     """
 
-    def __init__(self,
-                 config,
-                 xs,
-                 ys,
-                 initial_state=None,
-                 criterion=nn.BCELoss(),
-                 optimizer=optim.Adadelta,
-                 nEpochs=100,
-                 device=None):
+    def __init__(
+        self,
+        config,
+        xs,
+        ys,
+        initial_state=None,
+        criterion=nn.BCELoss(),
+        optimizer=optim.Adadelta,
+        nEpochs=100,
+        device=None,
+    ):
         self.logger = logging.getLogger("experimenter.backproppop")
         self.reporters = ReporterSet()
         self.config = config
 
         USE_CUDA = torch.cuda.is_available()
-        USE_CUDA = False
+        # USE_CUDA = False
         self.device = torch.device("cuda:1" if USE_CUDA else "cpu")
 
         if not type(xs) is torch.Tensor:
@@ -71,29 +74,30 @@ class BackpropPopulation(Population):
 
         self.nEpochs = nEpochs
 
-        stagnation = config.stagnation_type(
-            config.stagnation_config, self.reporters)
-        self.reproduction = config.reproduction_type(config.reproduction_config,
-                                                     self.reporters,
-                                                     stagnation)
+        stagnation = config.stagnation_type(config.stagnation_config, self.reporters)
+        self.reproduction = config.reproduction_type(
+            config.reproduction_config, self.reporters, stagnation
+        )
 
-        if config.fitness_criterion == 'max':
+        if config.fitness_criterion == "max":
             self.fitness_criterion = max
-        elif config.fitness_criterion == 'min':
+        elif config.fitness_criterion == "min":
             self.fitness_criterion = min
-        elif config.fitness_criterion == 'mean':
+        elif config.fitness_criterion == "mean":
             self.fitness_criterion = mean
         elif not config.no_fitness_termination:
             raise RuntimeError(
-                "Unexpected fitness_criterion: {0!r}".format(config.fitness_criterion))
+                "Unexpected fitness_criterion: {0!r}".format(config.fitness_criterion)
+            )
 
         if initial_state is None:
             # Create a population from scratch, then partition into species.
-            self.population = self.reproduction.create_new(config.genome_type,
-                                                           config.genome_config,
-                                                           config.pop_size)
+            self.population = self.reproduction.create_new(
+                config.genome_type, config.genome_config, config.pop_size
+            )
             self.species = config.species_set_type(
-                config.species_set_config, self.reporters)
+                config.species_set_config, self.reporters
+            )
             self.generation = 0
             self.species.speciate(config, self.population, self.generation)
         else:
@@ -102,8 +106,7 @@ class BackpropPopulation(Population):
         self.best_genome = None
 
     def backpropagate(self, xs, ys, nEpochs=5):
-        self.logger.info(
-            'about to start backprop with {} epochs'.format(nEpochs))
+        self.logger.info("about to start backprop with {} epochs".format(nEpochs))
         try:
             nEpochs = self.config.generations_of_backprop
         except AttributeError:
@@ -115,8 +118,7 @@ class BackpropPopulation(Population):
 
             # net = NeatNet(genome, self.config, criterion=self.criterion)
             try:
-                net = nneat(genome, self.config,
-                            criterion=nn.BCEWithLogitsLoss())
+                net = nneat(genome, self.config, criterion=nn.BCEWithLogitsLoss())
             except GenomeNotValidError:
                 ("This net - {} isn't valid".format(k))
                 preBPLoss = 0
@@ -163,55 +165,54 @@ class BackpropPopulation(Population):
             # self.logger.info(net.genome.connections[ix])
             postLosses.append(postBPLoss.item())
 
-        self.logger.info('mean improvement: %s' % mean(improvements))
-        self.logger.info('best improvement: %s' % min(improvements))
-        self.logger.info('best loss: %s' % min(postLosses))
+        self.logger.info("mean improvement: %s" % mean(improvements))
+        self.logger.info("best improvement: %s" % min(improvements))
+        self.logger.info("best loss: %s" % min(postLosses))
 
     def run(self, fitness_function, n=None, nEpochs=100):
-        """
-
-        """
+        """ """
 
         if self.config.no_fitness_termination and (n is None):
             raise RuntimeError(
-                "Cannot have no generational limit with no fitness termination")
+                "Cannot have no generational limit with no fitness termination"
+            )
 
         k = 0
         while n is None or k < n:
             k += 1
 
-            with MethodTimer('generationStart'):
+            with MethodTimer("generationStart"):
                 self.reporters.start_generation(self.generation)
 
-            with MethodTimer('pre_backprop'):
-                self.reporters.pre_backprop(
-                    self.config, self.population, self.species)
+            with MethodTimer("pre_backprop"):
+                self.reporters.pre_backprop(self.config, self.population, self.species)
 
-            with MethodTimer('backprop'):
+            with MethodTimer("backprop"):
                 self.backpropagate(self.xs, self.ys, nEpochs=nEpochs)
 
-            with MethodTimer('post_backprop'):
-                self.reporters.post_backprop(
-                    self.config, self.population, self.species)
+            with MethodTimer("post_backprop"):
+                self.reporters.post_backprop(self.config, self.population, self.species)
 
-            logging.debug('The current population after backpropagation is')
+            logging.debug("The current population after backpropagation is")
             logging.debug(self.population)
 
             # Evaluate all genomes using the user-provided function.
             # fitness_function(list(iter(self.population.iteritems())), self.config)
 
-            with MethodTimer('evaluate fitness'):
-                fitness_function(self.population, self.config,
-                                 self.xs, self.ys, self.device)
+            with MethodTimer("evaluate fitness"):
+                fitness_function(
+                    self.population, self.config, self.xs, self.ys, self.device
+                )
 
             # Gather and report statistics.
             best = None
             for genome_id, g in self.population.items():
                 if best is None or g.fitness > best.fitness:
                     best = g
-            with MethodTimer('post evaluate'):
+            with MethodTimer("post evaluate"):
                 self.reporters.post_evaluate(
-                    self.config, self.population, self.species, best)
+                    self.config, self.population, self.species, best
+                )
 
             # Track the best genome ever seen.
             if self.best_genome is None or best.fitness > self.best_genome.fitness:
@@ -220,25 +221,28 @@ class BackpropPopulation(Population):
             if not self.config.no_fitness_termination:
                 # End if the fitness threshold is reached.
                 fv = self.fitness_criterion(
-                    g.fitness for genome_id, g in self.population.items())
+                    g.fitness for genome_id, g in self.population.items()
+                )
                 if fv >= self.config.fitness_threshold:
-                    self.reporters.found_solution(
-                        self.config, self.generation, best)
+                    self.reporters.found_solution(self.config, self.generation, best)
                     break
 
             # Create the next generation from the current generation.
 
-            with MethodTimer('pre_reproduction'):
+            with MethodTimer("pre_reproduction"):
                 self.reporters.pre_reproduction(
-                    self.config, self.population, self.species)
+                    self.config, self.population, self.species
+                )
 
-            with MethodTimer('reproduction'):
-                self.population = self.reproduction.reproduce(self.config, self.species,
-                                                              self.config.pop_size, self.generation)
+            with MethodTimer("reproduction"):
+                self.population = self.reproduction.reproduce(
+                    self.config, self.species, self.config.pop_size, self.generation
+                )
 
-            with MethodTimer('post reproduction'):
+            with MethodTimer("post reproduction"):
                 self.reporters.post_reproduction(
-                    self.config, self.population, self.species)
+                    self.config, self.population, self.species
+                )
 
             # Check for complete extinction.
             if not self.species.species:
@@ -247,29 +251,31 @@ class BackpropPopulation(Population):
                 # If requested by the user, create a completely new population,
                 # otherwise raise an exception.
                 if self.config.reset_on_extinction:
-                    self.population = self.reproduction.create_new(self.config.genome_type,
-                                                                   self.config.genome_config,
-                                                                   self.config.pop_size)
+                    self.population = self.reproduction.create_new(
+                        self.config.genome_type,
+                        self.config.genome_config,
+                        self.config.pop_size,
+                    )
                 else:
                     raise CompleteExtinctionException()
 
             # Divide the new population into species.
 
-            with MethodTimer('speciate'):
-                self.species.speciate(
-                    self.config, self.population, self.generation)
+            with MethodTimer("speciate"):
+                self.species.speciate(self.config, self.population, self.generation)
 
-            with MethodTimer('end generation'):
+            with MethodTimer("end generation"):
                 self.reporters.end_generation(
-                    self.config, self.population, self.species)
+                    self.config, self.population, self.species
+                )
 
             self.generation += 1
 
-        self.reporters.end_experiment(
-            self.config, self.population, self.species)
+        self.reporters.end_experiment(self.config, self.population, self.species)
 
         if self.config.no_fitness_termination:
             self.reporters.found_solution(
-                self.config, self.generation, self.best_genome)
+                self.config, self.generation, self.best_genome
+            )
 
         return self.best_genome
